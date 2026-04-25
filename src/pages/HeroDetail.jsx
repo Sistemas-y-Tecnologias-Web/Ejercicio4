c1b1cea30c4075f7b994fb4162b630f4fc2037e1
JonathanTubac <javiertubac1290.e@gmail.com>
2026-04-25 03:22:16 -0600

    style: added some padding and changed title of the page

--- src/pages/HeroDetail.jsx
+++ src/pages/HeroDetail.jsx
@@ -11,16 +11,45 @@ export default function HeroDetail() {
 
     if (error) return <p>Error al cargar</p>
 
-    console.log(hero)
+    const getYouTubeId = (url) => {
+        try {
+            const parsed = new URL(url)
+
+            if (parsed.searchParams.get("v")) {
+                return parsed.searchParams.get("v")
+            }
+
+            const parts = parsed.pathname.split("/")
+            return parts.filter(Boolean).pop()
+        } catch {
+            return null
+        }
+    }
+
+    const videoId = getYouTubeId(hero.story.media.link)
+
     return (
         <>
             <header>
                 <h1 className="agent-title" style={
-                    {backgroundImage: `url(${hero.backgrounds[2].url})`}
+                    { backgroundImage: `url(${hero.backgrounds[2].url})` }
                 }>{hero.name}</h1>
                 <p className="about">{hero.description}</p>
                 <h2>Abilities</h2>
-                <TabContainer hero={hero}/>
+                <TabContainer hero={hero} />
+                <h2>{hero.name} story</h2>
+                <p className="about">{hero.story.summary}</p>
+
+                <iframe
+                    src={videoId ? `https://www.youtube.com/embed/${videoId}` : ""}
+                    frameBorder="0"
+                    width="100%"
+                    height="800"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Hero Video"
+                />
+                
             </header>
         </>
     )
